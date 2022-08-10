8dbda2e5e9bc668851c28a5f5b4665449fee7d9c
nifasabila <109188525+nifasabila@users.noreply.github.com>
2022-08-10 15:28:48 +0700

    Add files via upload

---
+++ tugas2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Tugas2</title>
+</head>
+<body>
+<?php
+$a =3;
+
+  for ($i , $a; $i <= 10; $i++){
+  if($i) {
+    echo pow ($a,$i)."<br>";
+  } 
+  }
+  ?>
+</body>
+</html>
